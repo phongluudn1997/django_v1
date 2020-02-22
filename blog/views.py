a84b5bd42a72f3e1ed088ebeda7fd42972e39431
Huynh Phong Luu <luu.huynh@fiisoft.com>
2020-02-22 10:23:20 +0700

    used view template and matching to the app, fake data and pass by variable called context.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post comment',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Secode post content',
+        'date_posted': 'February 21, 2020'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('Hello World, This is Blog.')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
